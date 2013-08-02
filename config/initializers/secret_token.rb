de2f07fe04810213d304e6004018949227d870d0
adityashedge <aditya@joshsoftware.com>
2013-08-02 15:52:20 +0530

    initial commit with complete presentation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JoshAggregationframework::Application.config.secret_key_base = 'aaa03f9c49461415b5e495cdde4a8acd85fd7376a96e079cf04f41c8c063d6b8731b86184faa3cf4f05d921a56154d61aedbe03e02aee23a6e816096dd48737c'
